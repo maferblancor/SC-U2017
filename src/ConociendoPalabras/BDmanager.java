4e0fa11b5577232700814966066e96fb1592d314
Carlos Alejandro Jimenez Holmquist <solracjh15@gmail.com>
2017-11-17 02:44:37 +0000

    test CVS desde IDEA

--- src/ConociendoPalabras/BDmanager.java
+++ src/ConociendoPalabras/BDmanager.java
@@ -16,30 +16,17 @@ import org.sqlite.*;
  *
  * @author Miguel Martinez
  */
-<<<<<<< HEAD
 class BDmanager {
     Connection conn = null;
     
     static Connection ConnectDB(){
         try{
+            //test
             Class.forName("org.sqlite.JDBC");
             //JOptionPane.showMessageDialog(null, "Database connected");
             return DriverManager.getConnection("jdbc:sqlite:db/testDB.db");
         }catch(Exception e){
             //JOptionPane.showMessageDialog(null, "Database not connected\n" + e.getMessage());
-=======
-public class BDmanager {
-    Connection conn = null;
-    
-    public static Connection ConnectDB(){
-        try{
-            Class.forName("org.sqlite.JDBC");
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\julio\\ULA\\Servicio Comunitario\\ConociendoPalabras\\db\\testDB.db");
-           // JOptionPane.showMessageDialog(null, "Database connected");
-            return conn;
-        }catch(Exception e){
-           // JOptionPane.showMessageDialog(null, "Database not connected");
->>>>>>> 13a3801f41ef303ecd6f2f6f0e47b4c62ab444b2
             return null;
         }
     }
