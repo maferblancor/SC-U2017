7f1e692ce3a2539a1f953bc20d27f157a6767279
Miguel Martinez <martinezmg19@hotmail.com>
2017-11-22 18:39:44 -0400

    Ultima actualizacion de gitignore
    Con el patrón que tiene .gitignore ahora mismo debería ser suficiente para que cualquiera con cualquier IDE tenga solo los archivos esenciales.

---
+++ src/ConociendoPalabras/Jugador.java
@@ -0,0 +1,43 @@
+package ConociendoPalabras;
+
+
+import java.lang.String;
+
+/**
+ *
+ * @author Miguel Martinez
+ * @version 0
+ */
+public class Jugador {
+
+    private String nombre;
+    private int puntaje;
+
+    //constructor
+    public Jugador(String nombre_) {
+        this.nombre = nombre_;
+        this.puntaje = 0;
+    }
+
+    //gets y sets
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String value) {
+        this.nombre = value;
+    }
+
+    public int getPuntaje() {
+        return this.puntaje;
+    }
+
+    public void setPuntaje(int value) {
+        this.puntaje = value;
+    }
+    
+    public void addPoint(){
+        this.puntaje++;
+    }
+
+}
---
+++ src/ConociendoPalabras/Palabra.java
@@ -0,0 +1,69 @@
+package ConociendoPalabras;
+
+
+import java.awt.*;
+import java.util.*;
+import java.lang.String;
+
+/**
+ *
+ * @author Miguel Martinez
+ * @version 0
+ */
+public class Palabra {
+
+    //Variables
+    private Image imagen;
+    private String urlImagen;
+    private String etiqueta;
+    private String nombre;
+    private Set<Integer> set;
+
+    //Constructor
+    public Palabra(String nombre_, String url_) {
+        this.nombre = nombre_;
+        this.urlImagen = url_;
+    }
+
+    //gets y sets
+    public Image getImagen() {
+        return this.imagen;
+    }
+
+    public void setImagen(Image value) {
+        this.imagen = value;
+    }
+
+    public String getURLImagen() {
+        return this.urlImagen;
+    }
+
+    public void setURLImagen(String value) {
+        this.urlImagen = value;
+    }
+
+    public String getEtiqueta() {
+        return this.etiqueta;
+    }
+
+    public void setEtiqueta(String value) {
+        this.etiqueta = value;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String value) {
+        this.nombre = value;
+    }
+
+    public Set getSet() {
+        return this.set;
+    }
+
+    public void setSet(Set value) {
+        this.set = value;
+    }
+
+}
